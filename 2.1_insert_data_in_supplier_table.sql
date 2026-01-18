bed846eec387d772dab03ac4cd0d27ac96b3cf80
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2026-01-18 12:22:23 +0200

    push

---
+++ 2.1_insert_data_in_supplier_table.sql
@@ -0,0 +1,13 @@
+INSERT INTO [SandavTonerDB].[dbo].[Supplier]
+(SupplierName, Email, Phone, Province, City, Country, YearsInBusiness)
+VALUES
+('TechSource Supplies','info@techsource.com','0711111111','Gauteng','Johannesburg','South Africa',8),
+('PrintPro Distributors','sales@printpro.com','0722222222','Gauteng','Pretoria','South Africa',12),
+('OfficeGear Ltd','contact@officegear.com','0733333333','Western Cape','Cape Town','South Africa',6),
+('MegaPrint Africa','support@megaprint.co.za','0744444444','KwaZulu-Natal','Durban','South Africa',10),
+('FastInk Suppliers','orders@fastink.com','0755555555','Limpopo','Polokwane','South Africa',5),
+('Smart Office Solutions','hello@smartoffice.com','0766666666','Mpumalanga','Nelspruit','South Africa',9),
+('Elite Toners','info@elitetoners.com','0777777777','Free State','Bloemfontein','South Africa',7),
+('QuickPrint Supplies','sales@quickprint.com','0788888888','Northern Cape','Kimberley','South Africa',4),
+('National Office Mart','contact@nom.co.za','0799999999','Eastern Cape','East London','South Africa',15),
+('UltraPrint Systems','support@ultraprint.com','0700000000','North West','Rustenburg','South Africa',11);
--- 2._create_supplier_table.sql
+++ 2._create_supplier_table.sql
@@ -2,14 +2,14 @@ USE [SandavTonerDB];
 
 IF OBJECT_ID('Supplier', 'U') IS NULL
 BEGIN
-    CREATE TABLE dbo.Supplier (
+    CREATE TABLE [SandavTonerDB].dbo.Supplier (
         SupplierID INT IDENTITY(1,1) PRIMARY KEY,
         SupplierName VARCHAR(100) NOT NULL,
         Email VARCHAR(100),
         Phone VARCHAR(30),
-        Location VARCHAR(100),
-        YearsInBusiness INT,
+        Province VARCHAR(50),
+        City VARCHAR(50),
         Country VARCHAR(50),
-        TypeOfConsumables VARCHAR(100)
+        YearsInBusiness INT
     );
 END
