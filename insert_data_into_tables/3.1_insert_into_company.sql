481a87208c6265649f0182f857956d49cebaba36
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2026-01-22 12:14:21 +0200

    formated sql

--- insert_data_into_tables/3.1_insert_into_company.sql
+++ insert_data_into_tables/3.1_insert_into_company.sql
@@ -1,3 +1,20 @@
-INSERT INTO Company (CompanyName, CompanyEmail, CompanyPhone, CompanyProvince, CompanyCity, CompanyCountry, CompanyYearsInBusiness)
-VALUES ('Sandav Toner Business', 'info@sandav.co.za', '0111234567', 'KwaZulu Natal', 'Nongoma', 'South Africa', 5);
-
+INSERT INTO
+    Company (
+        CompanyName,
+        CompanyEmail,
+        CompanyPhone,
+        CompanyProvince,
+        CompanyCity,
+        CompanyCountry,
+        CompanyYearsInBusiness
+    )
+VALUES
+    (
+        'Sandav Toner Business',
+        'info@sandav.co.za',
+        '0111234567',
+        'KwaZulu Natal',
+        'Nongoma',
+        'South Africa',
+        5
+    );
