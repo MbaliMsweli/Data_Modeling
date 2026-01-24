a2dee199f77f89d5fb3d85201a3bb8b0bd8d17bd
Mbali Msweli <mbalimsweli.nongalo@gmail.com>
2026-01-24 21:22:21 +0200

    push

--- joins.sql/inner_joins.sql
+++ joins.sql/inner_joins.sql
@@ -1,23 +1,63 @@
 USE [SandavTonerDB]
 
+----selecting tables I will need to create my view
+SELECT* FROM [dbo].[Supplier]
+
+SELECT * FROM[dbo].[Product]
+
+SELECT * FROM [dbo].[SupplierStock]
+
+-----creating a view to check the specific product and which supplier has it
+CREATE VIEW CheckingProductFromSupplier AS
+SELECT 
+p.SupplierID,
+p.ProductName,
+p.CompatiblePrinterModels,
+ss.SupplierQuantityAvailable,
+ss.SupplierLastDateUpdated
+FROM [dbo].[Product] AS p
+INNER JOIN [dbo].[SupplierStock] AS ss
+ON p.SupplierID = ss.SupplierID
+WHERE CompatiblePrinterModels LIKE '%HP 1010%';
+
+SELECT * FROM CheckingProductFromSupplier;
+
+
+----selecting tables I will need to create my view
 SELECT * FROM [dbo].[CompanyOrder];
 
 SELECT * FROM [dbo].[CompanyOrderItem]
 
+SELECT* FROM [dbo].[Supplier]
+
 ----checking order status for the orders that the company placed from the supplier and the total paid for the quantity that was ordered
+SELECT * FROM [dbo].[CompanyOrder];
+
+SELECT * FROM [dbo].[CompanyOrderItem]
+
+CREATE VIEW CompanyOrderStatus_TotalPaid AS
 SELECT 
 co.CompanyOrderID,
+co.supplierID,
 co.OrderStatus,
 co.OrderDate,
+coi.Quantity,
 SUM(coi.Quantity*coi.Price) as TotalPaid
 FROM [CompanyOrder] AS co
 INNER JOIN [CompanyOrderItem] AS coi
 ON co.CompanyOrderID = coi.CompanyOrderID
 GROUP BY 
 co.CompanyOrderID,
+co.SupplierID,
 co.OrderStatus,
-co.OrderDate;
+co.OrderDate,
+coi.Quantity;
 
+SELECT * FROM CompanyOrderStatus_TotalPaid;
+
+SELECT * FROM CompanyOrder_TotalPaid;
+
+SELECT * FROM CompanyOrder_TotalPaid;
 
 SELECT* FROM [dbo].[Customer]
 SELECT* FROM [dbo].[CustomerOrder]
